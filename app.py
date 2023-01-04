8b50aa7f93819b71880e71803b189d55811765d4
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-01-04 11:23:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from streamlit_lottie import st_lottie
 from bokeh.models.widgets import Div
 import openai
 import os
+import numpy as np
+from io import BytesIO
+import streamlit.components.v1 as components
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -23,6 +26,41 @@ def load_lottieurl(url):
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
+def audiorec_demo_app():
+
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    # Custom REACT-based component for recording client audio in browser
+    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
+    # specify directory and initialize st_audiorec object functionality
+    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+
+    # TITLE and Creator information
+    st.title('streamlit audio recorder')
+    st.markdown('Implemented by '
+        '[Stefan Rummer](https://www.linkedin.com/in/stefanrmmr/) - '
+        'view project source code on '
+        '[GitHub](https://github.com/stefanrmmr/streamlit_audio_recorder)')
+    st.write('\n\n')
+
+
+    # STREAMLIT AUDIO RECORDER Instance
+    val = st_audiorec()
+    # web component returns arraybuffer from WAV-blob
+    st.write('Audio data received in the Python backend will appear below this message ...')
+
+    if isinstance(val, dict):  # retrieve audio data
+        with st.spinner('retrieving audio-recording...'):
+            ind, val = zip(*val['arr'].items())
+            ind = np.array(ind, dtype=int)  # convert to np array
+            val = np.array(val)             # convert to np array
+            sorted_ints = val[ind]
+            stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
+            wav_bytes = stream.read()
+
+        # wav_bytes contains audio data in format to be further processed
+        # display audio data as received on the Python side
+        st.audio(wav_bytes, format='audio/wav')
 
 
 #  To get rid of the Streamlit branding stuff
@@ -60,6 +98,9 @@ with st.container():
             image_resp = openai.Image.create(prompt=ai_image_idea, n=4, size='256x256')
             st.image(image_resp["data"][0]["url"])
             st.image(image_resp["data"][1]["url"])
+            
+# --- Audio ---
+audiorec_demo_app()
         
 # --- Docs ---
 with st.container():
