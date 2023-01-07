8edc3c7a750c2c1b8a3f8ba60577e507df54d083
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-01-07 06:44:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#  pip install requests streamlit streamlit_lottie bokeh==2.4.1
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
@@ -27,38 +26,8 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
         
-def audiorec_demo_app():
-
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
-    # specify directory and initialize st_audiorec object functionality
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
-
-    # STREAMLIT AUDIO RECORDER Instance
-    val = st_audiorec()
-    # web component returns arraybuffer from WAV-blob
-    st.write('Audio data received in the Python backend will appear below this message ...')
-
-    if isinstance(val, dict):  # retrieve audio data
-        with st.spinner('retrieving audio-recording...'):
-            ind, val = zip(*val['arr'].items())
-            ind = np.array(ind, dtype=int)  # convert to np array
-            val = np.array(val)             # convert to np array
-            sorted_ints = val[ind]
-            stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-            wav_bytes = stream.read()
-
-        # wav_bytes contains audio data in format to be further processed
-        # display audio data as received on the Python side
-        st.audio(wav_bytes, format='audio/wav')
-
-
 #  To get rid of the Streamlit branding stuff
 local_css("css/styles.css")
-
-#  Anchor
-st.title("#")  # This anchor is needed for the page to start at the top when it is called.
 
 # --- INTRO ---
 
@@ -81,17 +50,16 @@ with st.container():
     col1, col2 = st.columns(2)
     with col1:
         st.header("Create AI Artwork")
-        ai_image_idea = st.text_input("Enter an Image to Create","two orange cats playing chess, oil painting",key="placeholder")
+        st.caption("Mote: Currently static image as I have it OpenAI API query limit")
+        ai_image_idea = st.text_input("Enter an Image to Create","two dogs playing chess, oil painting",key="placeholder")
         st.button("Generate Art 🏃",key="generate")
         
     with col2:
         with st.spinner('Generating your Art'):
-            image_resp = openai.Image.create(prompt=ai_image_idea, n=4, size='256x256')
-            st.image(image_resp["data"][0]["url"])
-            st.image(image_resp["data"][1]["url"])
-            
-# --- Audio ---
-audiorec_demo_app()
+            st.image("dalle-dogs.png")
+            #image_resp = openai.Image.create(prompt=ai_image_idea, n=4, size='256x256')
+            #st.image(image_resp["data"][0]["url"])
+            #st.image(image_resp["data"][1]["url"])
         
 # --- Docs ---
 with st.container():
